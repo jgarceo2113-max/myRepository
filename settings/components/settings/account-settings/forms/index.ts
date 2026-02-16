7b5592857e21dbbf9c5ac03be2cd7f6ac12fe4a9
jgarceo2113-max <jgarceo2113@iskwela.psau.edu.ph>
2026-02-16 02:50:00 -0500

    Delete settings/components/settings/account-settings directory

--- settings/components/settings/account-settings/forms/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./email.form";
-export * from "./password.form";
-export * from "./photo.form";
-export * from "./profile.form";
--- settings/components/settings/account-settings/schema/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./email.schema";
-export * from "./password.schema";
-export * from "./photo.schema";
-export * from "./profile.schema";
